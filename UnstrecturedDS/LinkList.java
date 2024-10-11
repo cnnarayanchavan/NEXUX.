b6582390376def5ad056da02341e6aa4b45f7f2b
cnnarayanchavan <cnnarayanchavan@gmail.com>
2024-10-11 23:46:54 +0530

    Started

---
+++ UnstrecturedDS/LinkList.java
@@ -0,0 +1,11 @@
+package UnstrecturedDS;
+
+public class LinkList {
+    public static void main(String[] args) {
+        int num = 12;
+        int num2 = 12;
+        System.out.println(num+num2);
+        System.out.println("Good Good!!");
+
+    }
+}
