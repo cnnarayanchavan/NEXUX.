1d2541d6accebf2cc56a77833460e808959778fd
cnnarayanchavan <cnnarayanchavan@gmail.com>
2024-10-12 16:57:14 +0530

    Added Interface for payment getway

--- AdvanceJava/Main.java
+++ AdvanceJava/Main.java
@@ -3,10 +3,23 @@ package RemoteRepoNexux.AdvanceJava;
 public class Main {
     public static void main(String[] args) {
         //all execution will be here only..
-        Animal animal = new Dog();
-        animal.makeSound();
 
-        animal = new Cat();
-        animal.makeSound();
+        //here we have reference of interface and the object of classes
+        Animal animal = new Dog();
+//        animal.makeSound();
+
+        animal = new Cat();  //here now animal variable is holding Cat object
+//        animal.makeSound();
+
+
+        //This is for decoupling example
+        PaymentGateway pal = new PayWithPayPal();
+        SelectMethod select = new SelectMethod(pal);
+        select.makePayment(67364.99);
+
+        PaymentGateway moon = new PayWithMoonPay();
+        select = new SelectMethod(moon);
+        select.makePayment(898989.88);
+
     }
 }
---
+++ AdvanceJava/PaymentGateway.java
@@ -0,0 +1,34 @@
+package RemoteRepoNexux.AdvanceJava;
+
+public interface PaymentGateway {
+
+    //method for payment platform..
+    public void chooseGateway (double amount);
+}
+
+class PayWithPayPal implements PaymentGateway {
+    public void chooseGateway (double amount) {
+        System.out.println("Amount "+amount+" paid with PayPal thank you.");
+    }
+}
+
+class PayWithMoonPay implements PaymentGateway {
+    public void chooseGateway (double amount) {
+        System.out.println("Amount "+amount+" paid with MoonPay thank you.");
+    }
+}
+
+//class for actually performing decoupling
+class SelectMethod {
+    private PaymentGateway payment;
+
+    //initialize using constructor
+    public SelectMethod (PaymentGateway payment) {
+        this.payment = payment;
+    }
+
+    //method for calling the actual object for payment.
+    public void makePayment (double amount) {
+        payment.chooseGateway(amount);
+    }
+}
