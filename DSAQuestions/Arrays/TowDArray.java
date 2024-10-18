06dbdf1fb677270342a2b8bcb024af6ed44a1fd4
cnnarayanchavan <cnnarayanchavan@gmail.com>
2024-10-18 20:33:55 +0530

    linear search in 2D array

--- DSAQuestions/Arrays/TowDArray.java
+++ DSAQuestions/Arrays/TowDArray.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class TowDArray {
     public static void main(String[] args) {
-        towDArray();
+        Scanner sc = new Scanner(System.in);
+        int targetElement = sc.nextInt();
+        towDArray(targetElement);
 
     }
 
     //FUN FOR DEFINING AND PRINTING 2D ARRAY.
-    public static void towDArray () {
+    public static void towDArray ( int targetElement ) {
         //for 2D array we need a matrix in which we have rows and cols so let's check it out.
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Rows : ");
@@ -25,10 +27,18 @@ public class TowDArray {
             }
         }
 
+
+
+
         //for printing elements from 2D array
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < cols; j++) {
+                //search for an element in 2D array.
+                if (arr[i][j] == targetElement) {
+                    System.out.println("Element found at index :["+i+"]["+j+"]");
+                }
                 System.out.print(arr[i][j] + " ");
+
             }
             System.out.println();
         }
