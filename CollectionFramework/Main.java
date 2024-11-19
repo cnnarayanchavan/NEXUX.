1f5cf3d8c33949d34f11319bfd0848ff8e4e869f
cnnarayanchavan <cnnarayanchavan@gmail.com>
2024-11-19 23:09:04 +0530

    Constructors all types

--- CollectionFramework/Main.java
+++ CollectionFramework/Main.java
@@ -2,15 +2,21 @@ package CollectionFramework;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student();
+        Student Narayana = new Student(12,9.80f,"Narayana");
+//        Narayana.greetStudent();
+        Student Hash = new Student(Narayana);
+        Hash.greetStudent();
+
 
         //access the variable and give them the values
-        student1.rollNumber = 21;
-        student1.firstName = "Narayana";
-        student1.marks = 90.80f;
+//        student1.rollNumber = 21;
+//        student1.firstName = "Narayana";
+//        student1.marks = 90.80f;
 
-        System.out.println(student1.rollNumber);
-        System.out.println(student1.marks);
-        System.out.println(student1.firstName);
+//        System.out.println(student1.rollNumber);
+//        System.out.println(student1.marks);
+//        System.out.println(student1.firstName);
+
+
     }
 }
--- CollectionFramework/Student.java
+++ CollectionFramework/Student.java
@@ -1,7 +1,35 @@
 package CollectionFramework;
 
 public class Student {
-    int rollNumber = 45;
-    float marks = 90f;
-    String firstName = "John";
+
+    int rollNumber;
+    float marks;
+    String firstName;
+
+    //initializing object using constructors
+    //parameterised constructor.
+    public Student (int rollNumber, float marks, String firstName) {
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+        this.firstName = firstName;
+    }
+
+    //non-parameterised constructor.
+    public Student () {
+        System.out.println("Student Constructor(non-Parameterised) called");
+        System.out.println("your roll number is: " + rollNumber);
+    }
+
+    //copy constructor
+    Student (Student other) {
+        this.rollNumber = other.rollNumber;
+        this.marks = other.marks;
+        this.firstName = other.firstName;
+    }
+
+    //function for greet and print details of students
+    public void greetStudent() {
+        System.out.println("Hey there Welcome "+firstName);
+        System.out.println("your roll number is "+rollNumber + " and you obtained "+marks+" marks.");
+    }
 }
